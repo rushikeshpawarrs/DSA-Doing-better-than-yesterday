dc522c98abd5ff4fb0423ea878d63d10988a02e0
Rushikesh Pawar <144884208+rushikeshpawarrs@users.noreply.github.com>
2025-09-01 18:18:09 +0530

    Create LC 210. Course Schedule 2.cpp

---
+++ LC 210. Course Schedule 2.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public: 
+    vector<int> topsBfs(int n, unordered_map<int , list<int> >& adjList){
+        vector<int> ans;
+        queue<int> q;
+        unordered_map<int, int> indegree;
+
+        //indegree calculation
+        for(auto i: adjList){
+            int src = i.first;
+            for(auto nbr: i.second){
+                indegree[nbr]++;
+            }
+        }
+
+        //put all nodes inside queue, which has indegree = 0
+        for(int i = 0; i<n;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        //bfs logic
+        while(!q.empty()){
+            int fNode = q.front();
+            q.pop();
+            ans.push_back(fNode);
+            for(auto nbr: adjList[fNode]){
+                indegree[nbr]--;
+                if(indegree[nbr] == 0){
+                    q.push(nbr);
+                }
+            }
+        }
+        if(ans.size() == n ) return ans;
+        else return {}; 
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int , list<int> > adjList;
+
+        for(auto task: prerequisites){
+            int u = task[0];
+            int v = task[1];
+            adjList[v].push_back(u);
+        }
+        vector<int> ans = topsBfs(numCourses, adjList);
+        return ans;
+    }
+};
