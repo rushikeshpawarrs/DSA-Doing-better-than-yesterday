1342f746db35fde8b0e7d91e3039d0b16823bff9
Rushikesh Pawar <144884208+rushikeshpawarrs@users.noreply.github.com>
2026-02-05 21:14:51 +0530

    Add constructTransformedArray method in Solution class
    Implement a method to construct a transformed array based on shifts.

--- Circular_Array_Traversing.cpp
+++ Circular_Array_Traversing.cpp
@@ -3,3 +3,19 @@
 //11%5 = 1;
 //11 indexes traverse on a 5 size array
 //LC 3379
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n, 0);
+
+        for(int i=0; i < n; i++){
+            int shift = nums[i] % n; //reducing the large shifts within 0..n-1 range
+            int newidx = (i + shift) % n;
+
+            if(newidx < 0)  newidx += n;
+            result[i] = nums[newidx];
+        }
+        return result;
+    }
+};
