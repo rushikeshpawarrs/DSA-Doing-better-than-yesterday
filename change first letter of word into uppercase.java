031ed1cc58a2d3a84ed768594414ec25d68e6422
Rushikesh Pawar <144884208+rushikeshpawarrs@users.noreply.github.com>
2026-02-18 16:26:44 +0530

    Add method to capitalize first letter of words
    Implement a method to capitalize the first letter of each word in a string, handling leading spaces.

---
+++ change first letter of word into uppercase.java
@@ -0,0 +1,17 @@
+//handles leading spaces also
+public static String upp(String str){
+        StringBuilder sb = new StringBuilder("");
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for(int i = 1; i < str.length(); i++){
+            if(str.charAt(i) == ' ' && i < str.length() - 1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
